f6b290e757328a20daf116f691dff6cfd7c0d01b
muchovae <e.muchova@gmail.com>
2017-05-25 13:54:19 +0200

    Update max.py

--- PYTHON1/max.py
+++ PYTHON1/max.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python
-from __future__ import division
-import math
 import sys
-import matplotlib.pyplot as plt
-import numpy as np
 
 print("Enter numbers")
 inp=input()
 list=inp.split()
 for i in range(len(list)):
-        list[i]=int.list[i]
-
+        list[i]=int(list[i])
 new_list=sorted(list)
 print(*new_list,sep='\n')
 print("Maximum je",new_list[-1])
